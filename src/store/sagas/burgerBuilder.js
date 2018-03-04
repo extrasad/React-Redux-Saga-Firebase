a45c0585e52644b2d9cb8956dee6b27f42cbc0ce
charlyjazz <carlosjazzc1@gmail.com>
2018-03-04 14:52:43 -0400

    feat(app): create app
    Signed-off-by: charlyjazz <carlosjazzc1@gmail.com>

---
+++ src/store/sagas/burgerBuilder.js
@@ -0,0 +1,16 @@
+import { put } from "redux-saga/effects";
+
+import axios from '../../axios-orders';
+import * as actions from '../actions';
+
+export function* initIngredientsSaga(action) {
+  try {
+    const response = yield axios.get(
+      'https://react-my-burger-46141.firebaseio.com/ingredients.json'
+    )
+    yield put(actions.setIngredients(response.data));
+  }
+  catch (error) {
+    yield put(actions.fetchIngredientsFailed());
+  }
+}
